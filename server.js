4137b1b98dbc237144e84f24f8d186c6b1525a9b
Artsiom Slabodnik <artem.slabodnik@vrpconsulting.com>
2019-11-04 17:42:31 +0300

    change the port of server js file

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
 
-app.listen(8080, function() {
-    console.log('Node app is running on port', 8080);
+app.listen(5000, function() {
+    console.log('Node app is running on port', 5000);
 });

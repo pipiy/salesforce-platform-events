6fc7573f30536f0d92e1927ea0388966ac81f1ed
Artsiom Slabodnik <artem.slabodnik@vrpconsulting.com>
2019-11-04 17:55:05 +0300

    changes

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
 
-app.listen(5000, function() {
+app.listen(8080, function() {
     console.log('Node app is running on port', 8080);
 });

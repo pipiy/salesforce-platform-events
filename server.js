12157364d1ecf20f9c9a2518260caf319fa3d1e1
Artsiom Slabodnik <artem.slabodnik@vrpconsulting.com>
2019-11-05 18:25:50 +0300

    last changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/Public"));
 app.listen(port, "0.0.0.0", function() {
 	console.log("Listening on Port 5000");
 });

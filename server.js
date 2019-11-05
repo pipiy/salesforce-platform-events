315ae444fc01fd6d7f4c29886fe48316936a8fc9
Artsiom Slabodnik <artem.slabodnik@vrpconsulting.com>
2019-11-05 18:18:25 +0300

    changes with server file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + "/public"));
+var port = process.env.PORT || 5000;
 
-app.listen(8080, function() {
-    console.log('Node app is running on port', 8080);
+app.use(express.static(__dirname + "/public"));
+app.listen(port, "0.0.0.0", function() {
+	console.log("Listening on Port 5000");
 });

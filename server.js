d23fd03b3d050fac8ba9b1696fa5eeb8eacf1dab
Artsiom Slabodnik <artem.slabodnik@vrpconsulting.com>
2019-11-06 14:29:56 +0300

    try to use heroku config var

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
-var clientId = process.env.clientId;
-console.log(clientId);
-
 app.use(express.static(__dirname + "/Public"));
 app.listen(port, "0.0.0.0", function() {
 	console.log("Listening on Port 5000");

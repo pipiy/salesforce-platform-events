f140880c9aa0bb73999a76a543dc5fb8200bb7d9
Artsiom Slabodnik <artem.slabodnik@vrpconsulting.com>
2019-11-06 16:19:08 +0300

    try to use heroku config var

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
+var clientId = process.env.CLIENT_ID;
+var redirectUri = "https://platform-app-events.herokuapp.com/oauthcallback.html";
+var proxyUrl = "https://node-salesforce-proxy.herokuapp.com/proxy/";
 
 app.use(express.static(__dirname + "/Public"));
 app.listen(port, "0.0.0.0", function() {
 	console.log("Listening on Port 5000");
+});
+
+jsforce.browser.init({
+	clientId: clientId,
+	redirectUri: redirectUri,
+	proxyUrl: proxyUrl
 });
